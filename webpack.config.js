e17cff8d963b65f71e131a39a2e470f25cbe6e27
Yutaka Sasaki <sasaki_yutaka@sskyu.local>
2016-01-20 16:53:31 +0900

    Remove console.log

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,4 @@
 require('babel-core/register');
 
 var env = process.env.NODE_ENV || 'development';
-
 module.exports = require('./configs/webpack/' + env);
-
-console.log();
-console.log();
-console.log('NODE_ENV: ', env);
-console.log();
-console.log();
